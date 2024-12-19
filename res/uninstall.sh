75d0e44919abd46105afefb427078b66dca25506
menghaocheng <menghaocheng@vclusters>
2024-12-19 11:03:06 +0800

    uninstall时自动停止vpkd服务，删除init.vpkd.rc脚本

--- res/uninstall.sh
+++ res/uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+stop vpkd
 rm -fr /data/local/tmp/plugin/bin/vpkd
 rm -fr /data/local/tmp/plugin/bin/vpick
-rm -fr /data/local/tmp/plugin/etc/init/init.vpick.rc
+rm -fr /data/local/tmp/plugin/etc/init/init.vpkd.rc
 rm -fr /data/local/tmp/plugin/meta/vpk
