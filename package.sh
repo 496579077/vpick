5ec440ddcb69d04be4ba9d234684e6014a787fe9
menghaocheng <menghaocheng@vclusters>
2024-12-20 10:12:04 +0800

    打包文件调整:tar.gz ==> tgz

--- package.sh
+++ package.sh
@@ -20,22 +20,22 @@ cp init.vpkd.rc $CACHE_DIR/$NAME/etc/init/
 cp chmod 644 $CACHE_DIR/$NAME/etc/init/init.vpkd.rc
 cp -r res/vpk $CACHE_DIR/$NAME/meta
 
-tar -zcvf $CACHE_DIR/vpick.tar.gz -C $CACHE_DIR/$NAME .
+tar -zcvf $CACHE_DIR/vpick.tgz -C $CACHE_DIR/$NAME .
 mkdir $BUILD_DIR/$NAME-$VERSION
-mv $CACHE_DIR/vpick.tar.gz $BUILD_DIR/$NAME-$VERSION
+mv $CACHE_DIR/vpick.tgz $BUILD_DIR/$NAME-$VERSION
 cp res/install.sh $BUILD_DIR/$NAME-$VERSION
 cp res/uninstall.sh $BUILD_DIR/$NAME-$VERSION
 chmod 777 $BUILD_DIR/$NAME-$VERSION/install.sh
 chmod 777 $BUILD_DIR/$NAME-$VERSION/uninstall.sh
 
-tar -zcvf $BUILD_DIR/$NAME-$VERSION.tar.gz -C $BUILD_DIR $NAME-$VERSION
+tar -zcvf $BUILD_DIR/$NAME-$VERSION.tgz -C $BUILD_DIR $NAME-$VERSION
 
 mkdir -p $DIST
-mv $BUILD_DIR/$NAME-$VERSION.tar.gz dist
+mv $BUILD_DIR/$NAME-$VERSION.tgz dist
 
 echo "************************************************************"
 echo "build vpick finished!"
-echo "target file: $DIST/$NAME-$VERSION.tar.gz"
+echo "target file: $DIST/$NAME-$VERSION.tgz"
 echo "************************************************************"
 
 VERSION_WIN64=$VERSION-win64
@@ -44,11 +44,11 @@ mkdir -p $BUILD_DIR/$NAME-$VERSION_WIN64/bin
 cp -r res/win64/* $BUILD_DIR/$NAME-$VERSION_WIN64/
 #cp res/win64/*.bat $BUILD_DIR/$NAME-$VERSION_WIN64/
 cp ../../../../../out/target/product/rk3588_docker/system/bin/vpick $BUILD_DIR/$NAME-$VERSION_WIN64/bin
-tar -zcvf $BUILD_DIR/$NAME-$VERSION_WIN64.tar.gz -C $BUILD_DIR $NAME-$VERSION_WIN64
-mv $BUILD_DIR/$NAME-$VERSION_WIN64.tar.gz dist
+tar -zcvf $BUILD_DIR/$NAME-$VERSION_WIN64.tgz -C $BUILD_DIR $NAME-$VERSION_WIN64
+mv $BUILD_DIR/$NAME-$VERSION_WIN64.tgz dist
 
 echo "************************************************************"
 echo "build win64 tools finished!"
-echo "target file: $DIST/$NAME-$VERSION_WIN64.tar.gz"
+echo "target file: $DIST/$NAME-$VERSION_WIN64.tgz"
 echo "************************************************************"
 
--- res/install.sh
+++ res/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p /data/local/tmp/plugin
-tar -zxvf vpick.tar.gz -C /data/local/tmp/plugin/
+tar -zxvf vpick.tgz -C /data/local/tmp/plugin/
 chmod 644 /data/local/tmp/plugin/etc/init/init.vpkd.rc
