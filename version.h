4e32056fbb186fcd6707cc5296b56bc7ff75e2a7
menghaocheng <menghaocheng@vclusters.com>
2025-01-11 01:38:34 +0800

    V1.0.8

--- version.h
+++ version.h
@@ -4,6 +4,10 @@
 // const std::string VERSION="1.0.2";
 // const std::string VERSION="1.0.3";
 // const std::string VERSION="1.0.4";
-const std::string VERSION="V1.0.7";
+// const std::string VERSION="V1.0.5";
+// const std::string VERSION="V1.0.6";
+// const std::string VERSION="V1.0.7";
+const std::string VERSION="V1.0.8";
+
 
 #endif
